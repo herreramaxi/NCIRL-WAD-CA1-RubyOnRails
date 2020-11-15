7d43186b1763391a396d70c33fda63324b9122e1
Maxi <herreramaxi@gmail.com>
2020-11-15 00:32:19 +0000

    Creating products xml project on ruby on rails

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,13 @@
+require 'nokogiri'
+class ProductsController < ApplicationController
+  def index
+       
+  path = File.join Rails.root, 'storage'
+  xmlPath  = File.join(path, 'Products.xml')
+  xsltPath  = File.join(path, 'ProductsTransformation.xsl')
+  document = Nokogiri::XML(File.read(xmlPath))
+  template = Nokogiri::XSLT(File.read(xsltPath))
+  
+  @transform= template.transform(document)
+  end
+end
---
+++ app/javascript/packs/products.js
@@ -0,0 +1,30 @@
+﻿window.onload = function() {
+    document.getElementById("searchField").onkeyup = function() { Search() };
+};
+
+function Search() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchField");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tableOuput");
+    var rows = table.getElementsByTagName("td");
+    for (i = 0; i < rows.length; i++) {
+        var cells = rows[i].parentNode.childNodes;
+        var foundInRow = false;
+        for (j = 0; j < cells.length; j++) {
+            var cellValue = cells[j].textContent || row.innerText;
+
+            if (cellValue) {
+                txtValue = cellValue;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    foundInRow = true;
+                }
+            }
+        }
+        if (foundInRow) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }
+    }
+}
